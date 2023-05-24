6397293713a2d2a8f4628b414836a76d2a2ad75e
+ <khalifnashon@gmail.com>
2023-05-24 14:21:26 +0300

    any type

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,4 @@
+// Declaring variables explicity
 var character = "mario";
 var age;
 var isLoggedIn;
@@ -6,6 +7,7 @@ age = 30;
 // isLoggedIn = 34
 isLoggedIn = true;
 // arrays
+// In the future this can only be an array of strings
 var ninjas = [];
 ninjas.push('ryu');
 ninjas.push('chun-li');
@@ -15,9 +17,31 @@ var mixed = [];
 mixed.push("Hello");
 mixed.push(false);
 mixed.push(20);
+// we don't use parenthesis when declaring the type of a normal variable
+// we only do that when declaring the type for arrays
 var uid;
+uid = 123;
+uid = "122";
 // objects
 var ninjaOne;
 ninjaOne = { name: "yoshi", age: 30 };
 var ninjaTwo;
 ninjaTwo = { name: "Ken", age: 20, beltColour: "black" };
+// Using the any types where the type of a variable can later change
+var marks = 23;
+marks = 25;
+console.log(marks);
+marks = "Hello Sandra";
+console.log(marks);
+var mixedAny = [];
+mixedAny.push(5);
+mixedAny.push('mario');
+mixedAny.push(false);
+console.log(mixedAny);
+var ninjaAny;
+ninjaAny = { name: 'yoshi', age: 25 };
+console.log(ninjaAny);
+ninjaAny = { name: 25, age: 'yoshi' };
+console.log(ninjaAny);
+// Note: This can easily lead to errors in the future
+// Using Any rips out all the benefits for using typescript
--- sandbox.ts
+++ sandbox.ts
@@ -1,3 +1,5 @@
+// Declaring variables explicity
+
 let character: string = "mario"
 let age: number;
 let isLoggedIn: boolean;
@@ -9,6 +11,7 @@ age = 30;
 isLoggedIn = true;
 
 // arrays
+// In the future this can only be an array of strings
 let ninjas: string[] = [];
 
 ninjas.push('ryu');
@@ -21,7 +24,12 @@ mixed.push("Hello")
 mixed.push(false)
 mixed.push(20)
 
+// we don't use parenthesis when declaring the type of a normal variable
+// we only do that when declaring the type for arrays
+
 let uid:string|number
+uid = 123
+uid = "122"
 
 // objects
 let ninjaOne: object;
@@ -35,5 +43,29 @@ let ninjaTwo: {
 
 ninjaTwo = { name: "Ken", age: 20, beltColour: "black"}
 
+// Using the any types where the type of a variable can later change
+let marks: any = 23;
 
+marks = 25
+console.log(marks)
+marks = "Hello Sandra"
+console.log(marks)
+
+let mixedAny: any[] = [];
+
+mixedAny.push(5);
+mixedAny.push('mario');
+mixedAny.push(false);
+console.log(mixedAny);
+
+let ninjaAny: { name: any, age: any };
+
+ninjaAny = { name: 'yoshi', age: 25 };
+console.log(ninjaAny);
+
+ninjaAny = { name: 25, age: 'yoshi' };
+console.log(ninjaAny);
+
+// Note: This can easily lead to errors in the future
+// Using Any rips out all the benefits for using typescript
 
