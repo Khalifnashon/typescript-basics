cabc377ba294fc0e433dc0541f6c9c3915baf11d
+ <khalifnashon@gmail.com>
2023-05-24 14:47:30 +0300

    ts configuration done

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,44 +1,45 @@
+"use strict";
 // Declaring variables explicity
-var character = "mario";
-var age;
-var isLoggedIn;
+let character = "mario";
+let age;
+let isLoggedIn;
 // age = "luigi"
 age = 30;
 // isLoggedIn = 34
 isLoggedIn = true;
 // arrays
 // In the future this can only be an array of strings
-var ninjas = [];
+let ninjas = [];
 ninjas.push('ryu');
 ninjas.push('chun-li');
 console.log(ninjas);
 // union types
-var mixed = [];
+let mixed = [];
 mixed.push("Hello");
 mixed.push(false);
 mixed.push(20);
 // we don't use parenthesis when declaring the type of a normal variable
 // we only do that when declaring the type for arrays
-var uid;
+let uid;
 uid = 123;
 uid = "122";
 // objects
-var ninjaOne;
+let ninjaOne;
 ninjaOne = { name: "yoshi", age: 30 };
-var ninjaTwo;
+let ninjaTwo;
 ninjaTwo = { name: "Ken", age: 20, beltColour: "black" };
 // Using the any types where the type of a variable can later change
-var marks = 23;
+let marks = 23;
 marks = 25;
 console.log(marks);
 marks = "Hello Sandra";
 console.log(marks);
-var mixedAny = [];
+let mixedAny = [];
 mixedAny.push(5);
 mixedAny.push('mario');
 mixedAny.push(false);
 console.log(mixedAny);
-var ninjaAny;
+let ninjaAny;
 ninjaAny = { name: 'yoshi', age: 25 };
 console.log(ninjaAny);
 ninjaAny = { name: 25, age: 'yoshi' };
